5ef06870bc7ebdc134e97f84f8774d435e8c8299
Douglas Cheong <dcheong@gatech.edu>
2017-04-08 20:15:26 -0400

    registration ui on login page. no functionality yet

--- webapp/src/components/login.js
+++ webapp/src/components/login.js
@@ -5,8 +5,10 @@ import { Router, Link } from 'react-router';
 
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
+import Dialog from 'material-ui/Dialog';
 
 
 export default class Login extends React.Component {
@@ -15,10 +17,21 @@ export default class Login extends React.Component {
         this.checkLogin = this.checkLogin.bind(this)
         this.handleUserChange = this.handleUserChange.bind(this)
         this.handlePassChange = this.handlePassChange.bind(this)
+        this.handleOpen = this.handleOpen.bind(this)
+        this.handleCancel = this.handleCancel.bind(this)
+        this.handleRegister = this.handleRegister.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+        this.handleRegPassChange = this.handleRegPassChange.bind(this)
+        this.handleRegUserChange = this.handleRegUserChange.bind(this)
+        this.handleAmazonIDChange = this.handleAmazonIDChange.bind(this)
         this.state = {
             login: false,
             username: '',
-            password: ''
+            password: '',
+            amazonID: '',
+            registerUser: '',
+            registerPass: '',
+            open: false
         }
     }
     checkLogin() {
@@ -34,7 +47,40 @@ export default class Login extends React.Component {
     handlePassChange(e) {
         this.setState({ password: e.target.value })
     }
+    handleOpen() {
+        this.setState({ open: true })
+    }
+    handleCancel() {
+        this.handleClose()
+    }
+    handleRegister() {
+
+    }
+    handleClose() {
+        this.setState({open: false})
+    }
+    handleRegPassChange(e) {
+        this.setState({ registerPass: e.target.value })
+    }
+    handleRegUserChange(e) {
+        this.setState({ registerUser: e.target.value })
+    }
+    handleAmazonIDChange(e) {
+        this.setState({ amazonID: e.target.value })
+    }
     render() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary
+                onTouchTap={this.handleCancel}
+                />,
+            <RaisedButton
+                label="Register"
+                primary
+                onTouchTap={this.handleRegister}
+                />
+        ]
         return (<div>
             <AppBar title="CookForMe" showMenuIconButton={false} />
             <div className="content">
@@ -42,13 +88,13 @@ export default class Login extends React.Component {
                     <CardTitle title="Login" />
                     <CardText>
                         <TextField
-                            hintText="Username"
+                            floatingLabelText="Username"
                             value={this.state.username}
                             onChange={this.handleUserChange}
                             fullWidth
                         />
                         <TextField 
-                            hintText="Username"
+                            floatingLabelText="Password"
                             value={this.state.password}
                             onChange={this.handlePassChange}
                             type="password"
@@ -59,8 +105,41 @@ export default class Login extends React.Component {
                             label="Login"
                             onClick={this.checkLogin}
                         />
+                        <FlatButton
+                            primary
+                            label="Register"
+                            onClick={this.handleOpen}
+                        />
                     </CardText>
                 </Card>
+                <Dialog
+                    title="Registration"
+                    actions={actions}
+                    modal
+                    open = {this.state.open}>
+                        <TextField
+                            floatingLabelText="AmazonID"
+                            value={this.state.amazonID}
+                            onChange={this.handleAmazonIDChange}
+                            fullWidth
+                        />
+                        <p>
+                            An AmazonID is a alphanumeric string associated with your Amazon Echo and your Amazon Account - it's what we use to find your Echo! Go to this link to find out what your AmazonID is, and paste it into the box above.
+                        </p>
+                        <TextField
+                            floatingLabelText="User Name"
+                            value={this.state.registerUser}
+                            onChange={this.handleRegUserChange}
+                            fullWidth
+                        />
+                        <TextField
+                            floatingLabelText="Password"
+                            value={this.state.registerPass}
+                            onChange={this.handleRegPassChange}
+                            type="password"
+                            fullWidth
+                        />
+                </Dialog>
             </div>
             </div>)
     }
